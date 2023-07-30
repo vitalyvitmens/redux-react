7d1c762b15a472a9c77397e706ca40ab0716980e
vitalyvitmens <otm_@tut.by>
2023-07-30 17:06:21 +0300

    add data in project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
-import { createStore } from 'redux'
+import { legacy_createStore as createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
